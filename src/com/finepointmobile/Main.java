7671dc9ff47ee0efbcbbeba60648d767e5fc2c31
Daniel Malone <danieljmalone@gmail.com>
2017-07-24 20:58:17 -0500

    make application show with GUI, label

--- src/com/finepointmobile/Main.java
+++ src/com/finepointmobile/Main.java
@@ -1,11 +1,24 @@
 package com.finepointmobile;
 
-public class Main {
+import java.awt.*;
+
+public class Main extends Frame {
+
+    public Main() {
+        setLayout(new FlowLayout());
+        Label label = new Label("First name:");
+        add(label);
+        setTitle("Daniel");
+        setSize(300, 300);
+        setVisible(true);
+    }
 
     public static void main(String[] args) {
         // write your code here
         System.out.println("Welcome!");
         User user = new User("Daniel", "Malone");
         System.out.println(user.getFirstName());
+
+        Main app = new Main();
     }
 }
