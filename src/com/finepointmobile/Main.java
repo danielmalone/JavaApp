214ec9f1d2987a58c2ba6330acca4b9ddee6d49a
Daniel Malone <danieljmalone@gmail.com>
2017-07-24 20:45:22 -0500

    initial commit

---
+++ src/com/finepointmobile/Main.java
@@ -0,0 +1,11 @@
+package com.finepointmobile;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Welcome!");
+        User user = new User("Daniel", "Malone");
+        System.out.println(user.getFirstName());
+    }
+}
